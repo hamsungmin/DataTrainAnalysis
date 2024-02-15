1f7d2219ce54ab1156fe92da89c2552434971f8f
hamsungmin <hsm1852@gmail.com>
2024-02-15 22:36:20 +0900

    초기 셋팅

---
+++ src/main/java/com/sungmin/singletrain_borad/SingleTrainBoradApplication.java
@@ -0,0 +1,13 @@
+package com.sungmin.singletrain_borad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SingleTrainBoradApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SingleTrainBoradApplication.class, args);
+    }
+
+}
